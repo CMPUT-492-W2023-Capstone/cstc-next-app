2569d1b3107edf27b1f83fc423d4278fc4d7f642
Dekr0 <dekr0.dk@gmail.com>
2023-03-25 02:21:13 -0600

    Misc

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA4ukuK2GCppTaFKOxrgLGwMTisDNhAGMU",
+    authDomain: "cmput492-cstc-2023.firebaseapp.com",
+    databaseURL: "https://cmput492-cstc-2023-default-rtdb.firebaseio.com",
+    projectId: "cmput492-cstc-2023",
+    storageBucket: "cmput492-cstc-2023.appspot.com",
+    messagingSenderId: "582479476685",
+    appId: "1:582479476685:web:491c679d77169fd97bb684",
+    measurementId: "G-BMN5Q9EJ7G"
+};
--- src/pages/data.js
+++ src/pages/data.js
@@ -1,12 +1,9 @@
 import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '@/config';
 import { getDatabase, ref, onValue } from 'firebase/database';
 
 
-const firebaseConfig = {
-
-};
-
 const app = initializeApp(firebaseConfig);
-
 const db = getDatabase(app);
 
+console.log('Connected to DB');
