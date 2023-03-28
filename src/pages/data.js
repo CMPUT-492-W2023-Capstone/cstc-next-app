e25fe6a2a2505c77d27751979508e81809ba273d
Dekr0 <dekr0.dk@gmail.com>
2023-03-28 03:01:23 -0600

    Added table view

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,16 +1,53 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '@/config';
 import { getDatabase, ref, onValue } from 'firebase/database';
+import { useState, useEffect, useRef } from 'react';
 
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const devRef = ref(db, 'dev/');
+
+function TrafficDataRow({ timestamp, total }) {
+    return (
+        <tr>
+            <td className='timestamp'>{ timestamp }</td>
+            <td className='total'>{ total } </td>
+        </tr>
+    );
+}
 
 export default function TrafficDataTable() {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+
+        onValue(devRef, (snapshot) => {
+            const data = snapshot.val();
+            const newRows = [];
+            for (const time in data) {
+                newRows.push({'timestamp': time, 'total': data[time].total});
+            }
+
+            setRows(newRows);
+        });
+
+    }, []);
+
     return (
-        <>
-            <p>Data Page</p>
-        </>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Timestamp</th>
+                    <th>Number of Vehicles</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map((row, index) => (
+                    <TrafficDataRow key={index} timestamp={row.timestamp} total={row.total} />
+                ))}
+            </tbody>
+        </table>
     );
 }
 
