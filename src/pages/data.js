e13c018c8c89cdae9681c6460f5452349b73d84b
tobiogun99 <71339903+tobiogun99@users.noreply.github.com>
2023-04-13 16:48:58 -0600

    Update data.js

--- src/pages/data.js
+++ src/pages/data.js
@@ -7,7 +7,7 @@ import { useState, useEffect, useRef } from 'react';
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const devRef = ref(db, '53,5428:-113,4974');
+const devRef = ref(db, '53,5210:-113,5324');
 
 function TrafficDataRow({ timestamp, total, car, bus, bicycle, motorcycle, truck, train }) {
     return (
